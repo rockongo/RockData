3dc7008f698727e91b828b7c7c09667a1ae43575
RocKongo <rcgomez1981@gmail.com>
2025-07-06 16:22:36 -0400

    Fix: set correct host and port for Render

--- app.py
+++ app.py
@@ -75,5 +75,9 @@ def get_equipos():
     except:
         return jsonify([])
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
